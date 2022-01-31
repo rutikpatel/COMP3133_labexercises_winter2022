b2632c1a4389b8c5c917881ef08c0d85c325a327
Rutik Patel <rutikpatel3660@gmail.com>
2022-01-30 20:11:42 -0500

    labEx 2

---
+++ labEx2/app/calculator.js
@@ -0,0 +1,15 @@
+exports.add = function(i, j) {
+	return i + j;
+};
+
+exports.multiply = function(i, j) {
+	return i * j;
+};
+
+exports.divide = function(i, j) {
+	return i / j;
+};
+
+exports.subtract = function(i, j) {
+	return i - j;
+};
