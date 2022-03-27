c8c342b7f842144be56adc17f97bdba26f48c7b6
Rutik Patel <rutikpatel3660@gmail.com>
2022-03-27 19:33:56 -0400

    add labex 07

---
+++ labEx7/my-app/src/app/input-format.directive.ts
@@ -0,0 +1,15 @@
+import { Directive , HostListener, ElementRef } from '@angular/core';
+
+@Directive({
+  selector: '[appInputFormat]'
+})
+export class InputFormatDirective {
+
+  constructor(private el: ElementRef) {
+    
+   }
+   @HostListener('blur') onBlur() {
+    this.el.nativeElement.value = this.el.nativeElement.value.toUpperCase();
+  }
+
+}
